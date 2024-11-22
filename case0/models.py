3bce8769406ebf93ef9c628e670f62ccba2876d6
emiresenov <110808673+emiresenov@users.noreply.github.com>
2024-11-22 09:59:28 +0100

    Data generation for case 0

---
+++ case0/models.py
@@ -0,0 +1,107 @@
+from functools import partial
+
+import jax.numpy as jnp
+from jax import lax, jit, grad, vmap, jacrev
+
+from jaxpi.models import ForwardIVP
+from jaxpi.evaluator import BaseEvaluator
+from jaxpi.utils import ntk_fn, flatten_pytree
+
+from matplotlib import pyplot as plt
+
+
+class Burgers1D(ForwardIVP):
+    def __init__(self, config, u0, t_star):
+        super().__init__(config)
+
+        self.u0 = u0
+        self.t_star = t_star
+
+        self.t0 = t_star[0]
+        self.t1 = t_star[-1]
+
+        # Predictions over a grid
+        self.u_pred_fn = vmap(self.u_net, (None, 0))
+        self.r_pred_fn = vmap(self.r_net, (None, 0))
+
+    def u_net(self, params, t):
+        z = jnp.stack([t])
+        u = self.state.apply_fn(params, z)
+        return u[0]
+
+    def grad_net(self, params, t):
+        u_t = grad(self.u_net, argnums=1)(params, t)
+        return u_t
+
+    def r_net(self, params, t):
+        u = self.u_net(params, t)
+        u_t = grad(self.u_net, argnums=1)(params, t)
+        return u_t + u
+
+    @partial(jit, static_argnums=(0,))
+    def res_and_w(self, params, batch):
+        "Compute residuals and weights for causal training"
+        # Sort time coordinates
+        t_sorted = batch[:, 0].sort()
+        r_pred = vmap(self.r_net, (None, 0))(params, t_sorted)
+        # Split residuals into chunks
+        r_pred = r_pred.reshape(self.num_chunks, -1)
+        l = jnp.mean(r_pred**2, axis=1)
+        w = lax.stop_gradient(jnp.exp(-self.tol * (self.M @ l)))
+        return l, w
+
+    @partial(jit, static_argnums=(0,))
+    def losses(self, params, batch):
+        # Initial condition loss
+        u_pred = self.u_net(params, self.t0)
+        ics_loss = jnp.mean((self.u0 - u_pred) ** 2)
+
+        # Residual loss
+        if self.config.weighting.use_causal == True:
+            l, w = self.res_and_w(params, batch)
+            res_loss = jnp.mean(l * w)
+        else:
+            r_pred = vmap(self.r_net, (None, 0))(params, batch[:, 0])
+            res_loss = jnp.mean((r_pred) ** 2)
+
+        loss_dict = {"ics": ics_loss, "res": res_loss}
+        return loss_dict
+
+
+    @partial(jit, static_argnums=(0,))
+    def compute_l2_error(self, params, u_test):
+        u_pred = self.u_pred_fn(params, self.t_star)
+        print(f'u_pred: {u_pred}, u_test: {u_test}')
+        error = jnp.linalg.norm(u_pred - u_test) / jnp.linalg.norm(u_test)
+        return error
+
+
+class Burgers1DEvaluator(BaseEvaluator):
+    def __init__(self, config, model):
+        super().__init__(config, model)
+
+    def log_errors(self, params, u_ref):
+        l2_error = self.model.compute_l2_error(params, u_ref)
+        self.log_dict["l2_error"] = l2_error
+
+    def log_preds(self, params):
+        u_pred = self.model.u_pred_fn(params, self.model.t_star)
+        fig = plt.figure(figsize=(6, 5))
+        plt.imshow(u_pred.T, cmap="jet")
+        self.log_dict["u_pred"] = fig
+        plt.close()
+
+    def __call__(self, state, batch, u_ref):
+        self.log_dict = super().__call__(state, batch)
+
+        if self.config.weighting.use_causal:
+            _, causal_weight = self.model.res_and_w(state.params, batch)
+            self.log_dict["cas_weight"] = causal_weight.min()
+
+        if self.config.logging.log_errors:
+            self.log_errors(state.params, u_ref)
+
+        if self.config.logging.log_preds:
+            self.log_preds(state.params)
+
+        return self.log_dict
