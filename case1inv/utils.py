cefe20388388670022588784ab68eae05823508f
emiresenov <110808673+emiresenov@users.noreply.github.com>
2024-11-29 14:37:58 +0100

    Define sol

---
+++ case1inv/utils.py
@@ -0,0 +1,20 @@
+import scipy.io
+import jax.numpy as jnp
+
+U = 5.0
+R_0 = 25.0
+R_1 = 0.5
+C_1 = 8.0
+
+t_end = 50.0
+n_samples = 100
+
+
+def solution(t):
+    return U/R_0 + (U/R_1)*jnp.exp(-t/(R_1*C_1))
+
+def get_dataset():
+    t = jnp.linspace(0.0, t_end, n_samples)
+    u = solution(t)
+    return u, t
+
