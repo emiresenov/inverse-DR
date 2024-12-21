7b3e064455612d98a47df3e1d95b4341a7afc0fd
emiresenov <110808673+emiresenov@users.noreply.github.com>
2024-12-21 20:03:29 +0100

    Loglog solution for Case 2

--- case2/utils.py
+++ case2/utils.py
@@ -6,27 +6,26 @@ import numpy as np
 
 #np.random.seed(42)
 
-V = 100.0
-R_0 = 100.0
-R_1 = 10.0
-C_1 = 1.0
+V = 5.0
+R_0 = 25.0
+R_1 = 0.3
+C_1 = 0.05
 
-R_2 = 50.0
-C_2 = 10.0
+R_2 = 5.0
+C_2 = 0.45
 
-t_end = 5000.0
-n_samples = 20000
+t_start = 0.0001
+t_end = 10000.0
+n_samples = 50
 
 
 def solution(t):
-    I_01 = V/R_0 + (V/R_1)*jnp.exp(-t/(R_1*C_1))
-    I_2 = V/R_2*jnp.exp(-t/(R_2*C_2))
-    return I_01 + I_2
+    I_01 = V/R_0 + (V/R_1)*jnp.exp(-jnp.power(10,t)/(R_1*C_1))
+    I_2 = V/R_2*jnp.exp(-jnp.power(10,t)/(R_2*C_2))
+    return jnp.log10(I_01+I_2)
 
 def get_dataset():
-    t = jnp.linspace(0.0, t_end, n_samples)
-    #noise = np.random.normal(0, 2, len(t))
-    #u = solution(t) + noise
+    t = jnp.linspace(jnp.log10(t_start), jnp.log10(t_end), n_samples)
     u = solution(t)
     return u, t
 
