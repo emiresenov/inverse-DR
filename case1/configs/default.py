ea9117b70d1530db20876cad78a959785e16f459
emiresenov <110808673+emiresenov@users.noreply.github.com>
2024-12-22 15:40:44 +0100

    Update default.py

--- case1/configs/default.py
+++ case1/configs/default.py
@@ -45,7 +45,7 @@ def get_config():
     # Weighting
     config.weighting = weighting = ml_collections.ConfigDict()
     weighting.scheme = "grad_norm"
-    weighting.init_weights = ml_collections.ConfigDict({"data": 0.1, "ics": 0.1, "res": 1.0})
+    weighting.init_weights = ml_collections.ConfigDict({"data": 1.0, "ics": 1.0, "res": 1.0})
     weighting.momentum = 0.9
     weighting.update_every_steps = 1000
 
