Fix Case 1, weighting crucial in loglog

case2loglog/configs/default.py
@@ -2,7 +2,6 @@ import ml_collections
 
 import jax.numpy as jnp
 
-
 def get_config():
     """Get the default hyperparameter configuration."""
     config = ml_collections.ConfigDict()
@@ -11,16 +10,16 @@ def get_config():
 
     # Weights & Biases
     config.wandb = wandb = ml_collections.ConfigDict()
-    wandb.project = "case 1 loglog"
-    wandb.name = "New expr"
+    wandb.project = "Case 2 loglog"
+    wandb.name = "2 layers"
     wandb.tag = None
 
     # Arch
     config.arch = arch = ml_collections.ConfigDict()
     arch.arch_name = "Mlp"
-    arch.num_layers = 1
+    arch.num_layers = 2
     arch.hidden_dim = 15
-    arch.out_dim = 1
+    arch.out_dim = 2 # I_01 and I_2
     arch.activation = "tanh"
     arch.periodicity = None
     arch.fourier_emb = None
@@ -39,13 +38,21 @@ def get_config():
 
     # Training
     config.training = training = ml_collections.ConfigDict()
-    training.max_steps = 30000
+    training.max_steps = 80000
     training.batch_size_per_device = 4096
 
     # Weighting
     config.weighting = weighting = ml_collections.ConfigDict()
     weighting.scheme = "grad_norm"
-    weighting.init_weights = ml_collections.ConfigDict({"data": 0.1, "ics": 0.1, "res": 1.0})
+    weighting.init_weights = ml_collections.ConfigDict(
+        {
+        "data": 0.1, 
+        "ic1": 0.1, 
+        "ic2": 0.1,
+        "res1": 1.0,
+        "res2": 1.0
+        }
+    )
     weighting.momentum = 0.9
     weighting.update_every_steps = 1000
 
@@ -60,9 +67,9 @@ def get_config():
 
     # Logging
     config.logging = logging = ml_collections.ConfigDict()
-    logging.log_every_steps = 1000
+    logging.log_every_steps = 500
     logging.log_errors = True
-    logging.log_losses = True
+    logging.log_losses = False
     logging.log_weights = False
     logging.log_grads = False
     logging.log_ntk = False
@@ -74,7 +81,9 @@ def get_config():
     inverse.params = {
         'R0' : jnp.array([1.]),
         'R1' : jnp.array([1.]),
-        'C1' : jnp.array([1.])
+        'C1' : jnp.array([1.]),
+        'R2' : jnp.array([1.]),
+        'C2' : jnp.array([1.]) 
     }
 
     # Input shape for initializing Flax models

case2loglog/models.py
@@ -1,25 +1,24 @@
 from functools import partial
 
 import jax.numpy as jnp
-from jax import lax, jit, grad, vmap, jacrev, tree_leaves
+from jax import lax, jit, grad, vmap, jacrev, tree_leaves, jacobian
 
 from jaxpi.models import InverseIVP
 from jaxpi.evaluator import BaseEvaluator
 from jaxpi.utils import ntk_fn, flatten_pytree
 
-from matplotlib import pyplot as plt
-
 from utils import V
 
+from matplotlib import pyplot as plt
 
-class CaseOne(InverseIVP):
+
+class CaseTwo(InverseIVP):
     def __init__(self, config, u_ref, t_star):
         super().__init__(config)
         self.t_star = t_star
         self.u_ref = u_ref
 
         self.t0 = t_star[0]
-        self.u0 = u_ref[0]
 
         # Vectorizing functions over multiple data points
         self.u_pred_fn = vmap(self.u_net, (None, 0))
@@ -29,79 +28,67 @@ class CaseOne(InverseIVP):
     def u_net(self, params, t):
         z = jnp.stack([t])
         u = self.state.apply_fn(params, z)
-        return u[0] # Unpack array
+        return u[0], u[1]
 
     # Calculate gradients
     def grad_net(self, params, t):
-        u_t = grad(self.u_net, argnums=1)(params, t)
-        return u_t
+        grads = jacobian(self.u_net, argnums=1)(params, t)
+        u1_t = grads[0]
+        u2_t = grads[1]
+        return u1_t, u2_t
 
     # Diff eq prediction (residual)
     def r_net(self, params, t):
-        u = self.u_net(params, t)
-        u_t = grad(self.u_net, argnums=1)(params, t)
+        u1, u2 = self.u_net(params, t)
+        u1_t, u2_t = self.grad_net(params, t)
         R0 = params['params']['R0']
         R1 = params['params']['R1']
         C1 = params['params']['C1']
-        tau = R1*C1
-        capped_exp = jnp.exp(jnp.clip(jnp.power(10, t) / tau, a_min=None, a_max=50))
-        return u_t + (R0*jnp.power(10,t))/(tau * (R1*capped_exp+R0))
-
-    @partial(jit, static_argnums=(0,))
-    def res_and_w(self, params, batch):
-        "Compute residuals and weights for causal training"
-        # Sort time coordinates
-        t_sorted = batch[:, 0].sort()
-        r_pred = vmap(self.r_net, (None, 0))(params, t_sorted)
-        # Split residuals into chunks
-        r_pred = r_pred.reshape(self.num_chunks, -1)
-        l = jnp.mean(r_pred**2, axis=1)
-        w = lax.stop_gradient(jnp.exp(-self.tol * (self.M @ l)))
-        return l, w
+        R2 = params['params']['R2']
+        C2 = params['params']['C2']
+        capped_exp = jnp.exp(jnp.clip(jnp.power(10, t) / R1*C1, a_min=None, a_max=50))
+        expr1 = u1_t + (R0*jnp.power(10,t))/(R1*C1*(R1*capped_exp+R0))
+        expr2 = u2_t + jnp.power(10,t)/(R2*C2)
+        return expr1, expr2
 
     @partial(jit, static_argnums=(0,))
     def losses(self, params, batch):
-        '''
-        Question: which initial condition loss do we use?
-        1: data measurement at t0 - ic squared
-        2: model prediction at t0 - ic squared
-        '''
         # Initial condition loss
         R0 = params['params']['R0']
         R1 = params['params']['R1']
-        ic = jnp.log10(V/R0 + V/R1)
-        u0_pred = self.u_net(params, self.t0) # Alternative: use self.u0
-        ics_loss = jnp.mean((u0_pred - ic) ** 2)
-        #ics_loss = jnp.mean((self.u0 - ic) ** 2)
+        R2 = params['params']['R2']
+        ic1 = jnp.log10(V/R0 + V/R1)
+        ic2 = jnp.log10(V/R2)
+        u0_pred_1, u0_pred_2 = self.u_net(params, self.t0)
+        ic1_loss = jnp.mean((u0_pred_1 - ic1) ** 2)
+        ic2_loss = jnp.mean((u0_pred_2 - ic2) ** 2)
 
         # Residual loss
-        if self.config.weighting.use_causal == True:
-            l, w = self.res_and_w(params, batch)
-            res_loss = jnp.mean(l * w)
-        else:
-            r_pred = vmap(self.r_net, (None, 0))(params, batch[:, 0])
-            res_loss = jnp.mean((r_pred) ** 2)
+        r1_pred, r2_pred = vmap(self.r_net, (None, 0))(params, batch[:, 0])
+        res1_loss = jnp.mean((r1_pred) ** 2)
+        res2_loss = jnp.mean((r2_pred) ** 2)
 
         # Data loss
-        u_pred = self.u_pred_fn(params, self.t_star)
+        u1_pred, u2_pred = self.u_pred_fn(params, self.t_star)
+        u_pred = u1_pred + u2_pred
         data_loss = jnp.mean((self.u_ref - u_pred) ** 2)
 
         #l1_penalty = 1e-1 * sum(jnp.sum(jnp.abs(w)) for w in tree_leaves(params))
         #loss_dict = {"data": data_loss + l1_penalty, "ics": ics_loss, "res": res_loss}
-        
-        loss_dict = {"data": data_loss, "ics": ics_loss, "res": res_loss}
 
+        loss_dict = {"data": data_loss, "ic1": ic1_loss, "ic2": ic2_loss, "res1": res1_loss, "res2": res2_loss}
         return loss_dict
 
 
     @partial(jit, static_argnums=(0,))
     def compute_l2_error(self, params, u_test):
-        u_pred = self.u_pred_fn(params, self.t_star)
+        u1_pred, u2_pred = self.u_pred_fn(params, self.t_star)
+        u_pred = u1_pred + u2_pred
         error = jnp.linalg.norm(u_pred - u_test) / jnp.linalg.norm(u_test)
         return error
 
 
-class CaseOneEvaluator(BaseEvaluator):
+class CaseTwoEvaluator(BaseEvaluator):
     def __init__(self, config, model):
         super().__init__(config, model)
 
@@ -110,10 +97,10 @@ class CaseOneEvaluator(BaseEvaluator):
         self.log_dict["l2_error"] = l2_error
 
     def log_preds(self, params):
-        u_pred = self.model.u_pred_fn(params, self.model.t_star)
+        u_pred_1, u_pred_2 = self.model.u_pred_fn(params, self.model.t_star)
         fig = plt.figure(figsize=(6, 5))
         plt.scatter(self.model.t_star, self.model.u_ref, s=50, alpha=0.9, c='orange')
-        plt.plot(self.model.t_star, u_pred, linewidth=8, c='black')
+        plt.plot(self.model.t_star, u_pred_1 + u_pred_2, linewidth=8, c='black')
         self.log_dict["u_pred"] = fig
         plt.close()
     
@@ -121,6 +108,8 @@ class CaseOneEvaluator(BaseEvaluator):
         self.log_dict["R0"] = params['params']['R0'][0]
         self.log_dict["R1"] = params['params']['R1'][0]
         self.log_dict["C1"] = params['params']['C1'][0]
+        self.log_dict["R2"] = params['params']['R2'][0]
+        self.log_dict["C2"] = params['params']['C2'][0]
         
 
     def __call__(self, state, batch, u_ref):
@@ -140,4 +129,3 @@ class CaseOneEvaluator(BaseEvaluator):
             self.log_inv_params(state.params)
 
         return self.log_dict
-    

case2loglog/utils.py
@@ -8,18 +8,21 @@ R0 = 25.0
 R1 = 0.5
 C1 = 8.0
 
+R2 = 0.1
+C2 = 0.1
+
 t_start = 0.0001
 t_end = 10000.0
-n_samples = 50
+n_samples = 80
 
 
 def solution(t):
-    return jnp.log10(V/R0 + (V/R1)*jnp.exp(-jnp.power(10,t)/(R1*C1)))
+    expr1 = V/R0 + (V/R1)*jnp.exp(-jnp.power(10,t)/(R1*C1))
+    expr2 = (V/R2)*jnp.exp(-jnp.power(10,t)/(R2*C2))
+    return jnp.log10(expr1 + expr2)
 
 def get_dataset():
     t = jnp.linspace(jnp.log10(t_start), jnp.log10(t_end), n_samples)
-    #noise = np.random.normal(0, 2, len(t))
-    #u = solution(t) + noise
     u = solution(t)
     return u, t
 
@@ -27,8 +30,3 @@ def get_dataset():
 
 
 
-#print(get_dataset())
-
-
-#print(np.linspace(0.1, t_end, n_samples))
-#print(np.log10(np.linspace(0.001, t_end, n_samples)))