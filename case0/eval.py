ad437a734d21b609c0dc08cf35e42404c440010f
emiresenov <110808673+emiresenov@users.noreply.github.com>
2024-12-27 16:38:48 +0100

    Case 0 eval

--- case0/eval.py
+++ case0/eval.py
@@ -8,6 +8,7 @@ from utils import get_dataset
 import wandb
 import pandas as pd
 import seaborn as sns
+import matplotlib.lines as mlines
 
 from utils import R, C
 
@@ -101,22 +102,33 @@ def evaluate(config: ml_collections.ConfigDict, workdir: str):
 
     fig, axes = plt.subplots(1, 2, figsize=(8, 4), sharex=True)
 
-    sns.lineplot(data=df, x=df.index, y='R', label='Estimated $R$', ax=axes[0], linewidth=2.5, color='#1f77b4')
-    axes[0].axhline(R, color='magenta', linestyle='--', label='True $R$', linewidth=2.5)
+    linecol = 'deepskyblue'
+    dashcolor = '#BB00BB'
+
+    sns.lineplot(data=df, x=df.index, y='R', label='Estimated $R$', ax=axes[0], linewidth=3, color=linecol)
+    axes[0].axhline(R, color=dashcolor, linestyle='--', label='True $R$', linewidth=3, dashes=(5, 6))  # Adjust dashes here
     axes[0].set_xlabel('Training step')
     axes[0].set_ylabel('Resistance $R$ ($\Omega$)')
-    axes[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.25), frameon=False)
     axes[0].grid(visible=True, which='major', linestyle='--', linewidth=0.5, alpha=0.5)
 
-    sns.lineplot(data=df, x=df.index, y='C', label='Estimated $C$', ax=axes[1], linewidth=2.5, color='#1f77b4')
-    axes[1].axhline(C, color='magenta', linestyle='--', label='True $C$', linewidth=2.5)
+    sns.lineplot(data=df, x=df.index, y='C', label='Estimated $C$', ax=axes[1], linewidth=3, color=linecol)
+    axes[1].axhline(C, color=dashcolor, linestyle='--', label='True $C$', linewidth=3, dashes=(5, 6))  # Adjust dashes here
     axes[1].set_xlabel('Training step')
     axes[1].set_ylabel('Capacitance $C$ (farad)')
-    axes[1].legend(loc='upper center', bbox_to_anchor=(0.5, 1.25), frameon=False)
     axes[1].grid(visible=True, which='major', linestyle='--', linewidth=0.5, alpha=0.5)
 
 
-    '''Doing this manually because dynamic handling is a headache'''
+    # Custom Legend
+    true_r = mlines.Line2D([], [], color=dashcolor, linestyle=(0, (3.5, 2)), linewidth=2.5, label='True $R$')  # Custom dashed line for legend
+    true_c = mlines.Line2D([], [], color=dashcolor, linestyle=(0, (3.5, 2)), linewidth=2.5, label='True $C$')  # Custom dashed line for legend
+    estimated_r = mlines.Line2D([], [], color=linecol, linewidth=2.5, label='Estimated $R$')
+    estimated_c = mlines.Line2D([], [], color=linecol, linewidth=2.5, label='Estimated $C$')
+
+    axes[0].legend(handles=[estimated_r, true_r], loc='upper center', bbox_to_anchor=(0.5, 1.25), frameon=False)
+    axes[1].legend(handles=[estimated_c, true_c], loc='upper center', bbox_to_anchor=(0.5, 1.25), frameon=False)
+
+
+    '''Doing this manually because dynamic handling is messy'''
     '''# Update x-axis ticks for both plots
     x_ticks = [0, 100, 200, 300, 400]  # Original tick positions
     x_labels = ['0', '10k', '20k', '30k', '40k']  # New labels
