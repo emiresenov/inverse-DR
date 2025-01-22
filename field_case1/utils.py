40391151d5761c67ad545bea0e30f13eb2025776
emiresenov <110808673+emiresenov@users.noreply.github.com>
2025-01-22 12:29:18 +0100

    dataset creation

--- field_case1/utils.py
+++ field_case1/utils.py
@@ -1,7 +1,6 @@
 import jax.numpy as jnp
 import numpy as np
-import jax.tree as tree
-
+import jax.tree_util as tree
 
 np.random.seed(42)
 
@@ -12,7 +11,7 @@ C1 = 0.007
 
 t_start = 0.0
 t_end = 2.0
-n_samples = 25
+n_samples = 5
 
 e = 1.602e-19
 k = 8.617e-5
@@ -22,25 +21,31 @@ L = 1
 a = 0.5
 A = 1
 
+
 def calc_R0(T):
-    return L/(a*A*jnp.exp(-(e*W)/(k*T)))
+    return L / (a * A * jnp.exp(-(e * W) / (k * T)))
 
-def solution(t):
-    return V/R0 + (V/R1)*jnp.exp(-t/(R1*C1))
 
-def get_dataset():
+def solution(t, T):
+    return V / calc_R0(T) + (V / R1) * jnp.exp(-t / (R1 * C1))
+
+
+def get_dataset(T):
     t = jnp.linspace(t_start, t_end, n_samples)
-    #noise = np.random.normal(0, 2, len(t))
-    #u = solution(t) + noise
-    u = solution(t)
-    return u, t
+    u = solution(t, T)
+    Ts = jnp.ones((n_samples,)) * T
+    return jnp.array([u, t, Ts])
 
-def update_subnet(params: dict, weights: list):
-    leaves, structure = tree.flatten(params)
-    assert len(leaves) == len(weights)
-    updated_params = tree.unflatten(structure, weights)
-    return updated_params
+datasets = []
 
+Temperatures = [293.0, 313.0, 333.0]
 
+for T in Temperatures:
+    dataset = get_dataset(T)
+    datasets.append(dataset)
 
-print(calc_R0(293))
+arr = jnp.stack(datasets, axis=0)
+
+print(arr)
+
+print(f'{arr[1][0]=}')
